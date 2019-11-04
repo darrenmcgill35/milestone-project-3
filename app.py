ac7522bb634c11516f93de0c27a7df1ba430e93a
darrenmcgill35 <darrenmcgill@live.co.uk>
2019-11-04 21:10:52 +0000

    updated app.py

--- app.py
+++ app.py
@@ -1,30 +1,35 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+app.secret_key = 'darren_secret'
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        flash("Thanks, You have subscribed")
     return render_template("index.html")
 
 
-@app.route('/review')
+@app.route('/review',methods=["GET", "POST"])
 def review():
-    return render_template("review.html")
+    return render_template("review.html", page_title="Reviews")
 
 
-@app.route('/add_a_player')
+@app.route('/add_a_player',methods=["GET", "POST"])
 def add_a_player():
-    return render_template("add_a_player.html")
+    return render_template("add_a_player.html", page_title="Add a Player")
 
-@app.route('/review_a_player')
+
+@app.route('/review_a_player',methods=["GET", "POST"])
 def review_a_player():
-    return render_template("review_a_player.html")
+    return render_template("review_a_player.html", page_title="Review a Player")
 
-@app.route('/merchandise')
+
+@app.route('/merchandise',methods=["GET", "POST"])
 def merchandise():
-    return render_template("merchandise.html")
+    return render_template("merchandise.html", page_title="Merchandise")
 
 
 if __name__ == '__main__':
