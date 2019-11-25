1a783f50518b8c119a228cd2dba9b99f7490a5e1
darrenmcgill35 <darrenmcgill@live.co.uk>
2019-11-25 18:39:13 +0000

    update review_a_player.html

--- static/js/grayscale.js
+++ static/js/grayscale.js
@@ -43,10 +43,10 @@
 
 function initMap() {
             var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 3,
+                zoom: 6,
                 center: {
-                    lat: 46.619261,
-                    lng: -33.134766
+                    lat: 53.733300,
+                    lng: -7.710280
                 }
             });
 
