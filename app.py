7041d00b8a8abd3394f25677b3f9e310b27175f8
darrenmcgill35 <darrenmcgill@live.co.uk>
2019-12-10 20:35:47 +0000

    update app.py

--- app.py
+++ app.py
@@ -92,4 +92,4 @@ def merchandise():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
